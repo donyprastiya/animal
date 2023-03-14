d2ecdb61c6afd86d19574ffe70dbefe637ba1adc
Dony Prastiya <dony.nugraha@peruri.co.id>
2023-03-14 09:57:31 +0700

    Initial Commit

---
+++ sesi3-peer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sesi3-peer
+//
+//  Created by Dony Prastiya on 14/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
